b68b34ee9a4fe977041fc47feaa146d620e8d2bc
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-12-08 01:00:27 +0200

    fixed when news dont have img

--- Client/src/Components/Home/LatestNew/SingleNew.jsx
+++ Client/src/Components/Home/LatestNew/SingleNew.jsx
@@ -3,7 +3,14 @@ const SingleNew = ({ title, urlToImage, description, url }) => {
     <>
       <div className="col-md-4">
         <div className="infoareawrap">
-          <img src={urlToImage} alt={title} />
+          {urlToImage ? (
+            <img src={urlToImage} alt={title} />
+          ) : (
+            <img
+              src="https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"
+              alt={title}
+            />
+          )}
           <h1 className="text-center subtitle">{title}</h1>
           <p>{description}</p>
           <p className="text-center">
